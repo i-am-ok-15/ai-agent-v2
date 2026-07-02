777ddf61518fc4cc6dc2f5bc47f0834929869fd7
i-am-ok-15 <i-am-ok-15@github.com>
2026-07-02 14:53:35 +0100

    modularised all functions for client_setup(), user_input()

---
+++ config.py
@@ -0,0 +1,4 @@
+# Service Configurations
+MODEL = "openrouter/free"
+BASE_URL = "https://openrouter.ai/api/v1"
+
--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
+from config import *
 
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
+def client_setup():
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
 
-if not api_key:
-    raise RuntimeError("No OpenRouter API Key Found")
+    if not api_key:
+        raise RuntimeError("No OpenRouter API Key Found")
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
+    client = OpenAI(
+        base_url=BASE_URL,
+        api_key=api_key,
+    )
 
-model = "openrouter/free"
-messages=[
-    {
-        "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    }
-]
+    model = MODEL
 
-completion = client.chat.completions.create(model=model, messages=messages)
+    return client, model
+
+def get_completion(client, messages, model):
+    return client.chat.completions.create(model=model, messages=messages)
+
+def user_input():
+    parser = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages=[
+        {
+            "role": "user",
+            "content": args.user_prompt,
+        }
+    ]
+    return messages
 
 def verify_completion(completion):
     if not completion:
         return RuntimeError("Completion == None")
     return True
 
-def completion_printer(completion):
+def completion_printer(completion, messages):
     print(f"User Prompt: {messages[0]["content"]}")
     print(f"Prompt tokens: {completion.usage.prompt_tokens}")
     print(f"Response tokens: {completion.usage.completion_tokens}")
@@ -36,9 +48,12 @@ def completion_printer(completion):
     print(completion.choices[0].message.content)
 
 def main():
-    if verify_completion(completion):
-        completion_printer(completion)
+    client, model = client_setup()
+    messages = user_input()
+    completion = get_completion(client, messages, model)
 
+    if verify_completion(completion):
+        completion_printer(completion, messages)
     else:
         print("Failed to generate completion.")
 
